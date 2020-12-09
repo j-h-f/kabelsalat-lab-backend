7c6353435908fc292f35a67096cd6bae13d99701
Jan-Hendrik Fabricius <jan-hendrik.fabricius@t-online.de>
2020-12-09 17:08:16 +0100

    trying to enable es6 support in pm2

--- app.js
+++ app.js
@@ -10,4 +10,13 @@ app.get("/test", (req, res, next) => {
     res.send("This is a little test");
 })
 
+app.get("sensors", (req, res, next) => {
+    res.json({
+        sensors: [
+            {name: "temperature", unit: "°C"},
+            {name: "Humidity", unit: "%"}
+        ]
+    })
+})
+
 export default app;
